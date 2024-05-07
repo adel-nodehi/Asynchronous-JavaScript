224b799161df07e2c8330af9573aa86f087f3b38
Adel <adelnmo2003@gmail.com>
2024-05-07 19:48:49 +0330

    16-10 Chaning Promises

--- script.js
+++ script.js
@@ -116,6 +116,7 @@ setTimeout(() => {
 
 ///////////////////////////////////////
 // Consuming Promises
+// Chaining Promises
 
 // const getCountryData = function (country) {
 //   fetch(`https://restcountries.com/v3.1/name/${country}`)
@@ -128,10 +129,22 @@ setTimeout(() => {
 //       renderCountry(data[0]);
 //     });
 // };
+
 const getCountryData = function (country) {
+  // Country 1
   fetch(`https://restcountries.com/v3.1/name/${country}`)
     .then(response => response.json())
-    .then(data => renderCountry(data[0]));
+    .then(data => {
+      renderCountry(data[0]);
+      const neighbour = data[0].borders[0];
+
+      if (!neighbour) return;
+
+      // Country 2
+      return fetch(`https://restcountries.com/v3.1/alpha/${neighbour}`);
+    })
+    .then(response => response.json())
+    .then(data => renderCountry(data[0], 'neighbour'));
 };
 
 getCountryData('portugal');
