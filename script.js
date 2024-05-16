86c8c480e20502ce58ee75bf6bd0e4b18f5bcb72
Adel <adelnmo2003@gmail.com>
2024-05-16 20:04:38 +0330

    16-22 Running Promises in Parallel

--- script.js
+++ script.js
@@ -31,6 +31,14 @@ const renderCountry = function (data, className = '') {
   countriesContainer.style.opacity = 1;
 };
 
+const getJSON = function (url, errorMsg = 'something went wrong') {
+  return fetch(url).then(response => {
+    if (!response.ok) throw new Error(`${errorMsg} (${response.status})`);
+
+    return response.json();
+  });
+};
+
 ///////////////////////////////////////
 /* // Our First AJAX Call: XMLHttpRequest
 
@@ -346,7 +354,7 @@ whereAmI();
 */
 
 ///////////////////////////////////////
-// Returning Values from Async Functions
+/* // Returning Values from Async Functions
 
 const getPosition = function () {
   return new Promise(function (resolve, reject) {
@@ -412,3 +420,27 @@ console.log('1: Will get location');
 
   console.log('3: Finished getting location');
 })();
+*/
+
+///////////////////////////////////////
+// Running Promises in Parallel
+
+const get3Countries = async function (c1, c2, c3) {
+  try {
+    // const [data1] = await getJSON(`https://restcountries.com/v3.1/name/${c1}`);
+    // const [data2] = await getJSON(`https://restcountries.com/v3.1/name/${c2}`);
+    // const [data3] = await getJSON(`https://restcountries.com/v3.1/name/${c3}`);
+    // console.log([data1.capital, data2.capital, data3.capital].flat());
+
+    const data = await Promise.all([
+      getJSON(`https://restcountries.com/v3.1/name/${c1}`),
+      getJSON(`https://restcountries.com/v3.1/name/${c2}`),
+      getJSON(`https://restcountries.com/v3.1/name/${c3}`),
+    ]);
+
+    console.log(data.flatMap(d => d[0].capital));
+  } catch (err) {
+    console.error(err);
+  }
+};
+get3Countries('portugal', 'canada', 'tanzania');
